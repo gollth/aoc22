ecdf7008b4a75d21f1c5a0e9d7f1ea4c7d07e159
Thore Goll <thore.goll@franka.de>
2022-12-02 13:27:25 +0100

    [02] Solution B

--- 02/src/lib.rs
+++ 02/src/lib.rs
@@ -1,39 +1,66 @@
 use std::str::FromStr;
 
+use enum_iterator::{next_cycle, previous_cycle, Sequence};
 use itertools::Itertools;
 
+#[derive(Clone, Sequence)]
 pub enum Shape {
     Rock = 1,
     Paper = 2,
     Scissors = 3,
 }
 
+pub enum Outcome {
+    Loss = 0,
+    Draw = 3,
+    Win = 6,
+}
+
 impl Shape {
-    fn duell(&self, other: Self) -> u32 {
+    fn duell(&self, other: Self) -> Outcome {
         match (self, other) {
-            // draws
-            (Self::Rock, Self::Rock) => 3,
-            (Self::Paper, Self::Paper) => 3,
-            (Self::Scissors, Self::Scissors) => 3,
-            // losses
-            (Self::Rock, Self::Paper) => 0,
-            (Self::Paper, Self::Scissors) => 0,
-            (Self::Scissors, Self::Rock) => 0,
-            // wins
-            (Self::Rock, Self::Scissors) => 6,
-            (Self::Scissors, Self::Paper) => 6,
-            (Self::Paper, Self::Rock) => 6,
+            (Self::Rock, Self::Rock) => Outcome::Draw,
+            (Self::Paper, Self::Paper) => Outcome::Draw,
+            (Self::Scissors, Self::Scissors) => Outcome::Draw,
+
+            (Self::Rock, Self::Paper) => Outcome::Loss,
+            (Self::Paper, Self::Scissors) => Outcome::Loss,
+            (Self::Scissors, Self::Rock) => Outcome::Loss,
+
+            (Self::Rock, Self::Scissors) => Outcome::Win,
+            (Self::Scissors, Self::Paper) => Outcome::Win,
+            (Self::Paper, Self::Rock) => Outcome::Win,
+        }
+    }
+
+    fn opponent_plays_to_achieve(&self, outcome: Outcome) -> Self {
+        match outcome {
+            Outcome::Draw => self.clone(),
+            Outcome::Win => next_cycle(self).unwrap(),
+            Outcome::Loss => previous_cycle(self).unwrap(),
         }
     }
 }
 
 impl FromStr for Shape {
     type Err = ();
-    fn from_str(x: &str) -> Result<Shape, Self::Err> {
+    fn from_str(x: &str) -> Result<Self, Self::Err> {
         match x {
             "A" | "X" => Ok(Shape::Rock),
             "B" | "Y" => Ok(Shape::Paper),
             "C" | "Z" => Ok(Shape::Scissors),
+            _ => Err(()),
+        }
+    }
+}
+
+impl FromStr for Outcome {
+    type Err = ();
+    fn from_str(x: &str) -> Result<Self, Self::Err> {
+        match x {
+            "X" => Ok(Self::Loss),
+            "Y" => Ok(Self::Draw),
+            "Z" => Ok(Self::Win),
             _ => Err(()),
         }
     }
@@ -49,19 +76,46 @@ pub fn get_score(strategy_guide: &String) -> u32 {
                 .collect_tuple()
         })
         .flatten()
-        .map(|(opponent, yours)| yours.duell(opponent) + yours as u32)
+        .map(|(opponent, yours)| yours.duell(opponent) as u32 + yours as u32)
+        .sum()
+}
+
+pub fn get_score_b(strategy_guide: &String) -> u32 {
+    strategy_guide
+        .lines()
+        .map(|line| line.split_ascii_whitespace().collect_tuple())
+        .flatten()
+        .map(|(opponent, outcome)| {
+            (
+                Shape::from_str(opponent).unwrap(),
+                Outcome::from_str(outcome).unwrap(),
+            )
+        })
+        .map(|(opponent, outcome)| {
+            (
+                opponent.clone(),
+                opponent.opponent_plays_to_achieve(outcome),
+            )
+        })
+        .map(|(opponent, yours)| yours.duell(opponent) as u32 + yours as u32)
         .sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::get_score;
+    use crate::{get_score, get_score_b};
 
     #[test]
     fn test_sample_a() -> std::io::Result<()> {
         let content = std::fs::read_to_string("sample.txt")?;
-
         assert_eq!(get_score(&content), 15);
+        Ok(())
+    }
+
+    #[test]
+    fn test_sample_b() -> std::io::Result<()> {
+        let content = std::fs::read_to_string("sample.txt")?;
+        assert_eq!(get_score_b(&content), 12);
         Ok(())
     }
 }
--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,9 +1,13 @@
-use second::get_score;
+use second::{get_score, get_score_b};
 
 fn main() -> std::io::Result<()> {
     let strategy_guide = std::fs::read_to_string("input.txt")?;
 
     println!("Solution A");
     println!("Your score: {}", get_score(&strategy_guide));
+
+    println!("Solution B");
+    println!("Your score: {}", get_score_b(&strategy_guide));
+
     Ok(())
 }
