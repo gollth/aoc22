e283a3515e00d819684dfb12103c05d1731e6c29
Thore Goll <thoregoll@googlemail.com>
2022-12-29 11:59:58 +0100

    [20] Solution B

--- 20/src/lib.rs
+++ 20/src/lib.rs
@@ -4,15 +4,17 @@ use anyhow::{anyhow, Result};
 use rb::RingBuffer;
 use std::str::FromStr;
 
+type Number = i64;
+
 #[derive(Debug)]
 pub struct Sequence {
-    original: Vec<i32>,
-    sequence: RingBuffer<(usize, i32)>,
+    original: Vec<Number>,
+    sequence: RingBuffer<(usize, Number)>,
     step: usize,
 }
 
-impl FromIterator<i32> for Sequence {
-    fn from_iter<I: IntoIterator<Item = i32>>(iter: I) -> Self {
+impl FromIterator<Number> for Sequence {
+    fn from_iter<I: IntoIterator<Item = Number>>(iter: I) -> Self {
         let original = iter.into_iter().collect::<Vec<_>>();
         Self {
             original: original.clone(),
@@ -28,8 +30,8 @@ impl FromStr for Sequence {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let original = s
             .lines()
-            .map(|l| l.parse::<i32>().map_err(|e| anyhow!("{}", e)))
-            .collect::<Result<Vec<i32>>>()?;
+            .map(|l| l.parse::<Number>().map_err(|e| anyhow!("{}", e)))
+            .collect::<Result<Vec<_>>>()?;
         Ok(original.into_iter().collect())
     }
 }
@@ -50,15 +52,16 @@ impl Sequence {
         false
     }
 
-    pub fn values(&self) -> Vec<i32> {
+    pub fn values(&self) -> Vec<Number> {
         self.sequence.iter().map(|(_, x)| *x).collect()
     }
 
     pub fn mix(&mut self) {
         while !self.mix_step() {}
+        self.step = 0;
     }
 
-    pub fn coords(&self) -> (i32, i32, i32) {
+    pub fn coords(&self) -> (Number, Number, Number) {
         let zero = self
             .sequence
             .iter()
@@ -77,7 +80,7 @@ impl Sequence {
 mod tests {
     use super::*;
 
-    fn test_mix_step_n(n: usize, expectation: Vec<i32>) -> Result<()> {
+    fn test_mix_step_n(n: usize, expectation: Vec<Number>) -> Result<()> {
         let sample = std::fs::read_to_string("sample.txt")?;
         let mut sequence = Sequence::from_str(&sample)?;
 
@@ -127,6 +130,194 @@ mod tests {
         test_mix_step_n(7, vec![-2, 1, 2, -3, 4, 0, 3])
     }
 
+    fn test_mix_n(key: i64, rounds: usize, expectation: Vec<Number>) -> Result<()> {
+        let numbers = std::fs::read_to_string("sample.txt")?
+            .lines()
+            .map(|x| x.parse::<i64>().map_err(|e| anyhow!("{}", e)))
+            .map(|x| Ok(x? * key))
+            .collect::<Result<Vec<_>>>()?;
+        let mut sequence = Sequence::from_iter(numbers);
+
+        for _ in 0..rounds {
+            println!("{:?}", sequence.sequence);
+            sequence.mix();
+        }
+        println!("{:?}", sequence.sequence);
+        assert_eq!(sequence.values(), expectation);
+        Ok(())
+    }
+
+    const KEY: i64 = 811589153;
+
+    #[test]
+    fn sample_with_key1() -> Result<()> {
+        test_mix_n(
+            KEY,
+            1,
+            vec![
+                0,
+                -2434767459,
+                3246356612,
+                -1623178306,
+                2434767459,
+                1623178306,
+                811589153,
+            ],
+        )
+    }
+
+    #[test]
+    fn sample_with_key2() -> Result<()> {
+        test_mix_n(
+            KEY,
+            2,
+            vec![
+                0,
+                2434767459,
+                1623178306,
+                3246356612,
+                -2434767459,
+                -1623178306,
+                811589153,
+            ],
+        )
+    }
+
+    #[test]
+    fn sample_with_key3() -> Result<()> {
+        test_mix_n(
+            KEY,
+            3,
+            vec![
+                2434767459,
+                3246356612,
+                1623178306,
+                -1623178306,
+                -2434767459,
+                0,
+                811589153,
+            ],
+        )
+    }
+
+    #[test]
+    fn sample_with_key4() -> Result<()> {
+        test_mix_n(
+            KEY,
+            4,
+            vec![
+                1623178306,
+                -2434767459,
+                811589153,
+                2434767459,
+                3246356612,
+                -1623178306,
+                0,
+            ],
+        )
+    }
+
+    #[test]
+    fn sample_with_key5() -> Result<()> {
+        test_mix_n(
+            KEY,
+            5,
+            vec![
+                811589153,
+                -1623178306,
+                1623178306,
+                -2434767459,
+                3246356612,
+                2434767459,
+                0,
+            ],
+        )
+    }
+
+    #[test]
+    fn sample_with_keys6() -> Result<()> {
+        test_mix_n(
+            KEY,
+            6,
+            vec![
+                811589153,
+                -1623178306,
+                3246356612,
+                -2434767459,
+                1623178306,
+                2434767459,
+                0,
+            ],
+        )
+    }
+    #[test]
+    fn sample_with_keys7() -> Result<()> {
+        test_mix_n(
+            KEY,
+            7,
+            vec![
+                2434767459,
+                1623178306,
+                -1623178306,
+                811589153,
+                3246356612,
+                0,
+                -2434767459,
+            ],
+        )
+    }
+
+    #[test]
+    fn sample_with_keys8() -> Result<()> {
+        test_mix_n(
+            KEY,
+            8,
+            vec![
+                3246356612,
+                811589153,
+                -2434767459,
+                2434767459,
+                -1623178306,
+                0,
+                1623178306,
+            ],
+        )
+    }
+
+    #[test]
+    fn sample_with_keys9() -> Result<()> {
+        test_mix_n(
+            KEY,
+            9,
+            vec![
+                811589153,
+                1623178306,
+                -2434767459,
+                3246356612,
+                2434767459,
+                -1623178306,
+                0,
+            ],
+        )
+    }
+
+    #[test]
+    fn sample_with_keys10() -> Result<()> {
+        test_mix_n(
+            KEY,
+            10,
+            vec![
+                -2434767459,
+                1623178306,
+                3246356612,
+                -1623178306,
+                2434767459,
+                811589153,
+                0,
+            ],
+        )
+    }
+
     #[test]
     fn sample_coordinates() -> Result<()> {
         let sample = std::fs::read_to_string("sample.txt")?;
@@ -134,6 +325,23 @@ mod tests {
 
         sequence.mix();
         assert_eq!(sequence.coords(), (4, -3, 2));
+        Ok(())
+    }
+
+    #[test]
+    fn sample_coordinates_with_key() -> Result<()> {
+        let numbers = std::fs::read_to_string("sample.txt")?
+            .lines()
+            .map(|x| x.parse::<i64>().map_err(|e| anyhow!("{}", e)))
+            .map(|x| Ok(x? * KEY))
+            .collect::<Result<Vec<_>>>()?;
+        let mut sequence = Sequence::from_iter(numbers);
+
+        for _ in 0..10 {
+            sequence.mix();
+        }
+        assert_eq!(sequence.coords(), (811589153, 2434767459, -1623178306));
+
         Ok(())
     }
 
--- 20/src/main.rs
+++ 20/src/main.rs
@@ -1,6 +1,4 @@
-use std::str::FromStr;
-
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
 use twentieth::Sequence;
 
@@ -10,13 +8,29 @@ struct Options {
     /// Input file with the encrypted coordinates
     #[clap(long, default_value = "sample.txt")]
     file: String,
+
+    /// Which description key to use (1 for part 1, 811589153 for part 2)
+    #[clap(long, default_value_t = 1)]
+    key: i64,
+
+    /// How many rounds should the mixing happen?
+    #[clap(long, default_value_t = 1)]
+    rounds: usize,
 }
 
 fn main() -> Result<()> {
     let args = Options::parse();
-    let mut sequence = Sequence::from_str(&std::fs::read_to_string(&args.file)?)?;
+    let numbers = std::fs::read_to_string(&args.file)?
+        .lines()
+        .map(|x| x.parse::<i64>().map_err(|e| anyhow!("{}", e)))
+        .map(|x| Ok(x? * args.key))
+        .collect::<Result<Vec<_>>>()?;
 
-    sequence.mix();
+    let mut sequence = Sequence::from_iter(numbers);
+
+    for _ in 0..args.rounds {
+        sequence.mix();
+    }
 
     let coords = sequence.coords();
     println!("Decrypted coordinates: {:?}", coords);
